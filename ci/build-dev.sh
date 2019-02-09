c069730a3ee28d4036e88fc81ca1e46f1c1fa9e8
Antafes <map@wafriv.de>
2019-02-09 12:48:24 +0100

    Adjust path to the commit hash and add missing library to the dev build

--- ci/build-dev.sh
+++ ci/build-dev.sh
@@ -3,8 +3,15 @@
 apt-get update
 apt-get install zip
 
+# Get libraries
 mkdir lib
 cp -r ../myXML-lib/* lib
+wget http://mirror.checkdomain.de/apache//commons/lang/binaries/commons-lang3-3.8.1-bin.zip
+unzip commons-lang3-3.8.1-bin.zip
+cp commons-lang3-3.8.1/commons-lang3-3.8.1.jar lib
+rm -rf commons-lang3-*
+
+# Start build
 ant -Dfile.encoding=utf-8
 
 cd dist/
@@ -15,4 +22,4 @@ DATE=`date +%d%m%Y%H%M`
 echo "build-$DATE" >> ../dist/name
 TAG=`cat ../vampire-editor-pre-release/tag`
 echo "$TAG-$DATE" >> ../dist/tag
-cp ../vampire-editor-repo-dev/.git/ref ../dist/commit_sha
+cp .git/ref ../dist/commit_sha
--- ci/build.sh
+++ ci/build.sh
@@ -10,7 +10,7 @@ mkdir lib
 cp -r ../myXML-lib/* lib
 wget http://mirror.checkdomain.de/apache//commons/lang/binaries/commons-lang3-3.8.1-bin.zip
 unzip commons-lang3-3.8.1-bin.zip
-cp commons-lang3-3.8.1-bin/commons-lang3-3.8.1.jar lib
+cp commons-lang3-3.8.1/commons-lang3-3.8.1.jar lib
 rm -rf commons-lang3-*
 
 # Start build
