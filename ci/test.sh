2d5a050acf2f2b27c30edbedeb324104901dbdad
Antafes <map@wafriv.de>
2019-02-16 13:52:10 +0100

    Adjust test behaviour

--- ci/test.sh
+++ ci/test.sh
@@ -11,4 +11,25 @@ then
     ln -s "${M2_CACHE}" "${M2_HOME}"
 fi
 
+cat > ${M2_HOME}/settings.xml <<EOF
+
+<settings xmlns="http://maven.apache.org/SETTINGS/1.0.0"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0
+                          https://maven.apache.org/xsd/settings-1.0.0.xsd">
+    <profiles>
+        <profile>
+            <id>SUREFIRE-1588</id>
+            <activation>
+                <activeByDefault>true</activeByDefault>
+            </activation>
+            <properties>
+                <argLine>-Djdk.net.URLClassPath.disableClassPathURLCheck=true</argLine>
+            </properties>
+        </profile>
+    </profiles>
+</settings>
+
+EOF
+
 mvn test
