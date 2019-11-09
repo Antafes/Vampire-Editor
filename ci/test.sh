9d63e1b2a3a34c09502ac2bcb599c4bbe378c230
Antafes <map@wafriv.de>
2019-11-09 02:16:21 +0100

    Adjust CI pipeline

--- ci/test.sh
+++ ci/test.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+ACCESS_TOKEN="$1"
 ROOT_FOLDER="$( pwd )/../"
 M2_HOME="${HOME}/.m2"
 M2_CACHE="${ROOT_FOLDER}/maven"
@@ -11,25 +12,7 @@ then
     ln -s "${M2_CACHE}" "${M2_HOME}"
 fi
 
-cat > ${M2_HOME}/settings.xml <<EOF
-
-<settings xmlns="http://maven.apache.org/SETTINGS/1.0.0"
-      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-      xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0
-                          https://maven.apache.org/xsd/settings-1.0.0.xsd">
-    <profiles>
-        <profile>
-            <id>SUREFIRE-1588</id>
-            <activation>
-                <activeByDefault>true</activeByDefault>
-            </activation>
-            <properties>
-                <argLine>-Djdk.net.URLClassPath.disableClassPathURLCheck=true</argLine>
-            </properties>
-        </profile>
-    </profiles>
-</settings>
-
-EOF
+# Setup maven settings
+$( pwd )/ci/set-m2-settings.sh ${ACCESS_TOKEN}
 
 mvn test
