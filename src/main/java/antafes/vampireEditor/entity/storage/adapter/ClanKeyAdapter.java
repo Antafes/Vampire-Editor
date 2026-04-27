babb31161e573eaceb00d65346c4a27240037db5
Antafes <marian@pollzien.de>
2026-04-27 16:26:54 +0200

    Remove duplicate licence headers

--- src/main/java/antafes/vampireEditor/entity/storage/adapter/ClanKeyAdapter.java
+++ src/main/java/antafes/vampireEditor/entity/storage/adapter/ClanKeyAdapter.java
@@ -20,14 +20,6 @@
  * @license https://www.gnu.org/licenses/lgpl.html LGPLv3
  */
 
-/*
- * This file is part of Vampire Editor.
- *
- * Vampire Editor is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- */
 package antafes.vampireEditor.entity.storage.adapter;
 
 import antafes.vampireEditor.entity.character.Clan;
