1e9f514c5e74c50b287c77dda8afc2c71f061bea
Antafes <marian@pollzien.de>
2026-05-08 19:32:33 +0200

    Set willpower value during character creation

--- src/main/java/antafes/vampireEditor/entity/Character.java
+++ src/main/java/antafes/vampireEditor/entity/Character.java
@@ -293,6 +293,12 @@ public class Character extends BaseEntity {
             return this.self();
         }
 
+        public int getAdvantageValue(String key)
+        {
+            Advantage advantage = this.advantages.get(key);
+            return advantage != null ? advantage.getValue() : 0;
+        }
+
         public B addMerit(Merit merit) {
             this.merits.put(merit.getKey(), merit);
 
--- src/main/java/antafes/vampireEditor/gui/NewCharacterDialog.java
+++ src/main/java/antafes/vampireEditor/gui/NewCharacterDialog.java
@@ -535,6 +535,7 @@ public class NewCharacterDialog extends javax.swing.JDialog {
         this.abilitiesPanel.fillCharacter(builder);
         this.dialogDispatcher.dispatch(new FillCharacterEvent(builder));
         this.lastStepsPanel.fillCharacter(builder);
+        builder.setWillpower(builder.getAdvantageValue("courage"));
 
         ShowWaitAction waitAction = new ShowWaitAction(this);
         waitAction.show(aVoid -> {
